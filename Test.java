5ae823ae5fbdabb0a2ac22ea7016eaabdcaaefcf
Madhavi M <143279897+madhu553@users.noreply.github.com>
2026-04-27 18:29:58 +0530

    Add files via upload

---
+++ Test.java
@@ -0,0 +1,11 @@
+package com.soft.Test;
+
+import com.soft.entities.Employee;
+
+public class Test {
+	public static void main(String[]args) {
+		Employee emp=new Employee(111,20.5f);
+		emp.getemp();
+	}
+
+}
